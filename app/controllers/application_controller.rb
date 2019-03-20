c00743c5b30900e6949e6cf1b39026d33c5dd655
TAKAKING22 <oyo0222@gmail.com>
2019-03-20 16:03:54 +0900

    1.3.4 演習

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "Hello World!!"
   end
+
+  def hola
+    render html: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render html:  "goodbye, world!!"
+  end
 end
